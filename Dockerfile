afd456b4abe3e740322c88646e26411d11283162
Alexander Akpabio <lexyuko@gmail.com>
2024-07-15 15:42:17 +0100

    ready for deployment

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM openjdk:21-jdk-slim
+FROM openjdk:21-jdk
 LABEL authors="Michael"
 ARG JAR_FILE=target/*.jar
-COPY ./target/StarTaskz-0.0.1-SNAPSHOT.jar app.jar
-ENTRYPOINT ["java", "-jar", "/app.jar"]
+COPY ./target/StarTaskz-0.0.1-SNAPSHOT.jar StarTaskz-0.0.1-SNAPSHOT.jar
+ENTRYPOINT ["java", "-jar", "/StarTaskz-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/niit/StarTaskz/StarTaskzApplication.java
+++ src/main/java/com/niit/StarTaskz/StarTaskzApplication.java
@@ -1,14 +1,16 @@
 package com.niit.StarTaskz;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
-@EnableMongoRepositories(basePackages = "com.niit.startaskz.repository")
+
+@ComponentScan(basePackages = "com.niit.StarTaskz")
+@EnableMongoRepositories(basePackages = "com.niit.StarTaskz.repository")
 @SpringBootApplication
 public class StarTaskzApplication {
     private static final Logger logger = LoggerFactory.getLogger(StarTaskzApplication.class);
