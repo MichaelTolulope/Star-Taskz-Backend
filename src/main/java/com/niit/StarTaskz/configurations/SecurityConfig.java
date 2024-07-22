5891cac40057cca4b9a3b60394fd07bb7131ba4d
Michael Tolulope <olagunjum99@gmail.com>
2024-07-22 10:25:09 +0100

    cors error fixexd

--- src/main/java/com/niit/StarTaskz/configurations/SecurityConfig.java
+++ src/main/java/com/niit/StarTaskz/configurations/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.niit.StarTaskz.configurations;
 
-import org.apache.catalina.filters.HttpHeaderSecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,22 +7,42 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    protected SecurityFilterChain httpSecurityClient(HttpSecurity http) throws Exception {
-        return http
-                .authorizeRequests()
-                .anyRequest().permitAll()  // Allow all requests
-                .and().csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable()).build();// Disable CSRF protection for simplicity
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())); // Apply CORS configuration
+
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOriginPattern("*"); // Allow all origins
+        configuration.addAllowedHeader("*"); // Allow all headers
+        configuration.addAllowedMethod("*"); // Allow all methods
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-         return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
-
 }
