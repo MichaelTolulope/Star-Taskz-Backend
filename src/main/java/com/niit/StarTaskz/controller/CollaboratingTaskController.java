8017e0121cbf5ccbd27a0671b1cea9414f49e0bc
Michael Tolulope <olagunjum99@gmail.com>
2024-07-24 11:35:03 +0100

    message json serializer fixes

--- src/main/java/com/niit/StarTaskz/controller/CollaboratingTaskController.java
+++ src/main/java/com/niit/StarTaskz/controller/CollaboratingTaskController.java
@@ -17,7 +17,7 @@ public class CollaboratingTaskController {
     @Autowired
     CollaboratingTaskService taskService;
 
-    @PostMapping("/create-task{workspaceId}/{creatorId}")
+    @PostMapping("/create-task/{workspaceId}/{creatorId}")
     protected ResponseEntity<List<CollaboratingTask>> ceateCollabTask(@PathVariable String workspaceId,@PathVariable String creatorId,@RequestBody CollaboratingTask task){
        return new ResponseEntity<>(taskService.createTask(workspaceId,creatorId, task), HttpStatus.OK);
     }
--- src/main/java/com/niit/StarTaskz/model/collaboration_workspace/groups/Message.java
+++ src/main/java/com/niit/StarTaskz/model/collaboration_workspace/groups/Message.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@JsonSerialize(using = MessageSerializer.class)
+//@JsonSerialize(using = MessageSerializer.class)
 @Data
 public class Message {
     private String id;
